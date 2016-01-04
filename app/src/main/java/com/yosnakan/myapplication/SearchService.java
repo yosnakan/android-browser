dbe8561b5b68a5e89448daefa00bad31d5c3de96
yosnakan <yosnakan@yahoo.co.jp>
2016-01-05 00:57:10 +0900

    textboxを常時表示

--- app/src/main/java/com/yosnakan/myapplication/SearchService.java
+++ app/src/main/java/com/yosnakan/myapplication/SearchService.java
@@ -1,9 +1,14 @@
 package com.yosnakan.myapplication;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 /**
  * Created by yosnakan on 2016/01/04.
@@ -13,5 +18,23 @@ public class SearchService extends Service{
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private WindowManager mWindowManager;
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams mparams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        TextView textView = new TextView(this);
+        textView.setText("Hello World!!");
+
+        mWindowManager.addView(textView, mparams);
     }
 }
